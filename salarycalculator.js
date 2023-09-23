1558292cfdf1769a9f969240fd04085b4d563e25
GidyMane <muragegideon2000@gmail.com>
2023-09-23 07:14:43 -0700

    Added the studentGradeGenerator function

--- salarycalculator.js
+++ salarycalculator.js
@@ -1,1 +1,23 @@
 
+function studentGradeGenerator(marks){
+    if (marks > 79){
+        return "A"
+    }
+    else if(marks>=60 && marks<=79){
+        return "B"
+    }
+    else if(marks>=50 && marks<=59){
+        return "c"
+    }
+    else if(marks>=40 && marks <=49){
+        return "D"
+    }
+    else if(marks<40){
+        return "E"
+    }
+    else {
+        alert("Invalid input! Please enter a number between 0 and 100.")
+    }
+
+
+} 
